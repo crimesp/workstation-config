f4b7e99d164ee9edca101442ec7191d056dad3c6
Paul Crimes <paul.crimes@solirius.com>
2021-03-04 11:39:23 +0000

    added new git projects

--- bash/aliases/cd.sh
+++ bash/aliases/cd.sh
@@ -2,19 +2,15 @@
 # Directory shortcuts - bash etc
 ####################################
 alias .cd.aliases='cd $BASH_INCLUDES/aliases'
+alias .cd.workstation='cd ~/git/crimesp/workstation-config'
+alias ..cd.cloudservices='cd ~/git/cloudservices/'
+
+alias .cd.idea.projects='cd ~/idea/'
+alias .cd.multi='cd ~/idea/multiproject'
 
 alias .cd.em='cd ~/git/em'
-
 alias .cd.hrs='cd ~/git/em/em-hrs-api'
-
 alias .cd.ingestor='cd ~/git/em/em-hrs-ingestor'
+alias .cd.docstore='cd ~/git/em/document-management-store-app'
 
-alias .cd.workstation='cd ~/git/crimesp/workstation-config'
-
-alias .cd.idea.projects='cd /home/paul/idea/'
-
-alias .cd.multi='cd /home/paul/idea/multiproject'
-
-
-alias ..cd.cloudservices='cd ~/git/cloudservices/'
 
