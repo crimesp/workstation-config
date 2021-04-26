9f3408eb9b072e9b5fb62fcc7f9c625c4245b233
Paul Crimes <paul.crimes@solirius.com>
2021-04-26 14:42:24 +0100

    add context

--- bash/aliases/_new_aliases.sh
+++ bash/aliases/_new_aliases.sh
@@ -2,5 +2,9 @@
 
 alias .net.dns.dig='dig '
 
+alias .kubernetes.context.aat.00='kubectl config use-context aat-00-aks'
+alias .kubernetes.context.aat.01='kubectl config use-context aat-01-aks'
+alias .az.credentials.preview.00='az aks get-credentials --resource-group preview-00-rg --name preview-00-aks --subscription DCD-CFTAPPS-DEV --overwrite'
+
 alias .kubernetes.namespace.em='kubectl config set-context --current --namespace=em'
 alias .kubernetes.delete='kubectl delete pods'
