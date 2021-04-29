caf18bec65ca6c230b0fc37fe4243cd450820737
Paul Crimes <paul.crimes@solirius.com>
2021-04-29 13:26:13 +0100

    k8s

--- bash/aliases/_new_aliases.sh
+++ bash/aliases/_new_aliases.sh
@@ -4,7 +4,22 @@ alias .net.dns.dig='dig '
 
 alias .kubernetes.context.aat.00='kubectl config use-context aat-00-aks'
 alias .kubernetes.context.aat.01='kubectl config use-context aat-01-aks'
+
+alias .kubernetes.context.perftest.00='kubectl config use-context perftest-00-aks'
+alias .kubernetes.context.perftest.01='kubectl config use-context perftest-01-aks'
+
+
+alias .kubernetes.context.preview.00='kubectl config use-context preview-00-aks'
+alias .kubernetes.context.preview.01='kubectl config use-context preview-01-aks'
+
+preview-01-aks
+
+
 alias .az.credentials.preview.00='az aks get-credentials --resource-group preview-00-rg --name preview-00-aks --subscription DCD-CFTAPPS-DEV --overwrite'
+
+
+alias .az.credentials.perf.00='az aks get-credentials --resource-group perftest-00-rg --name perftest-00-aks --subscription DCD-CFTAPPS-TEST --overwrite'
+
 
 alias .kubernetes.namespace.em='kubectl config set-context --current --namespace=em'
 alias .kubernetes.delete='kubectl delete pods'
