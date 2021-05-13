8e4be2cdbdd6e8fec758ed759582967b8b6103be
Paul Crimes <paul.crimes@solirius.com>
2021-05-13 12:12:54 +0100

    cd flux

--- bash/aliases/cd.sh
+++ bash/aliases/cd.sh
@@ -12,5 +12,5 @@ alias .cd.em='cd ~/git/em'
 alias .cd.hrs='cd ~/git/em/em-hrs-api'
 alias .cd.ingestor='cd ~/git/em/em-hrs-ingestor'
 alias .cd.docstore='cd ~/git/em/document-management-store-app'
-
+alias .cd.flux='cd ~/git/em/cnp-flux-config'
 
