c24477d8c16bde736136c09c4b0e6d9fb96fa767
Paul Crimes <paul.crimes@solirius.com>
2021-07-19 20:08:46 +0100

    latest helpers

--- bash/aliases/kubernetes.sh
+++ bash/aliases/kubernetes.sh
@@ -17,8 +17,19 @@ alias .helm.list='helm list -a'
 
 
 
+alias .kubectl.describe.hr.ingestor='kubectl describe hr -n em em-hrs-ingestor'
+alias .kubectl.describe.hr.api='kubectl describe hr -n em em-hrs-api'
 
 
+
+alias .kubectl.describe.images.hrs.ingestor.context.all.perftest='...kubectl.describe.images.hrs.ingestor.context.all.perftest'
+
+function ...kubectl.describe.images.hrs.ingestor.context.all.perftest() {
+  .kubectl.context.perftest.00
+  kubectl describe pod -l app.kubernetes.io/name=em-hrs-ingestor-job | egrep "mage|Start|State|^Name:"
+  .kubectl.context.perftest.01
+  kubectl describe pod -l app.kubernetes.io/name=em-hrs-ingestor-job | egrep "mage|Start|State|^Name:"
+}
 
 
 
@@ -35,9 +46,9 @@ alias .kubectl.describe.images.hrs.api.context.all.perftest='...kubectl.describe
 
 function ...kubectl.describe.images.hrs.api.context.all.perftest() {
   .kubectl.context.perftest.00
-  kubectl describe pod -l app.kubectl.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
+  kubectl describe pod -l app.kubernetes.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
   .kubectl.context.perftest.01
-  kubectl describe pod -l app.kubectl.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
+  kubectl describe pod -l app.kubernetes.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
 }
 
 
@@ -58,10 +69,10 @@ alias .kubectl.context.show='kubectl config get-contexts'
 alias .kubectl.context.namespace.em='kubectl config set-context --current --namespace=em'
 
 
-alias .kubectl.delete.api.perf='.kubectl.context.perftest.00 && kubectl delete pods -l app.kubectl.io/name=em-hrs-api-java && .kubectl.context.perftest.01 && kubectl delete pods -l app.kubectl.io/name=em-hrs-api-java '
-alias .kubectl.delete.api='kubectl delete pods -l app.kubectl.io/name=em-hrs-api-java'
+alias .kubectl.delete.api.perf='.kubectl.context.perftest.00 && kubectl delete pods -l app.kubernetes.io/name=em-hrs-api-java && .kubectl.context.perftest.01 && kubectl delete pods -l app.kubernetes.io/name=em-hrs-api-java '
+alias .kubectl.delete.api='kubectl delete pods -l app.kubernetes.io/name=em-hrs-api-java'
 alias .kubectl.delete.hrs-all='kubectl delete pods -l in (em-hrs-api-java,em-hrs-ingestor-java)'
-alias .kubectl.delete.ingestor='kubectl delete pods -l app.kubectl.io/name=em-hrs-api-java'
+alias .kubectl.delete.ingestor='kubectl delete pods -l app.kubernetes.io/name=em-hrs-api-java'
 alias .kubectl.delete='kubectl delete pods'
 
 alias .kubectl.deployment.describe='kubectl describe deployment'
@@ -86,9 +97,9 @@ alias .kubectl.cronjob='kubectl get cronjobs'
 alias .kubectl.jobs.list='kubectl get jobs'
 
 
-alias .kubectl.log.all="kubectl logs -f --tail 500 -l 'app.kubectl.io/name in (em-hrs-api-java,em-hrs-ingestor-java,em-hrs-api-java-78dcfd6f49-9d48r,em-hrs-api-java-78dcfd6f49-cpq8t,em-hrs-api-java-78dcfd6f49-7c7tp,em-hrs-api-java-78dcfd6f49-rh7jq)'"
-alias .kubectl.log.api='kubectl logs -f --tail 500 -l app.kubectl.io/name=em-hrs-api-java'
-alias .kubectl.log.ingestor='kubectl logs -f --tail 500 -l app.kubectl.io/name=em-hrs-ingestor-java'
+alias .kubectl.log.all="kubectl logs -f --tail 500 -l 'app.kubernetes.io/name in (em-hrs-api-java,em-hrs-ingestor-java,em-hrs-api-java-78dcfd6f49-9d48r,em-hrs-api-java-78dcfd6f49-cpq8t,em-hrs-api-java-78dcfd6f49-7c7tp,em-hrs-api-java-78dcfd6f49-rh7jq)'"
+alias .kubectl.log.api='kubectl logs -f --tail 500 -l app.kubernetes.io/name=em-hrs-api-java'
+alias .kubectl.log.ingestor='kubectl logs -f --tail 500 -l app.kubernetes.io/name=em-hrs-ingestor-java'
 alias .kubectl.log.nofollow='kubectl logs'
 alias .kubectl.log='kubectl logs -f --tail 500'
 
@@ -101,9 +112,9 @@ alias .kubectl.traefic.admin='kubectl get service traefik -n admin -o json'
 
 function ...kubectl.describe.images.hrs.api.context.all.perftest() {
   .kubectl.context.perftest.00
-  kubectl describe pod -l app.kubectl.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
+  kubectl describe pod -l app.kubernetes.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
   .kubectl.context.perftest.01
-  kubectl describe pod -l app.kubectl.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
+  kubectl describe pod -l app.kubernetes.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
 }
 
 
@@ -111,9 +122,9 @@ function ...kubectl.describe.images.hrs.api.context.all.perftest() {
 alias .kubectl.describe.images.hrs.api.context.all.prod='...kubectl.describe.images.hrs.api.context.all.prod'
 function ...kubectl.describe.images.hrs.api.context.all.prod() {
   .kubectl.context.prod.00
-  kubectl describe pod -l app.kubectl.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
+  kubectl describe pod -l app.kubernetes.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
   .kubectl.context.prod.01
-  kubectl describe pod -l app.kubectl.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
+  kubectl describe pod -l app.kubernetes.io/name=em-hrs-api-java | egrep "mage|Start|State|^Name:"
 }
 
 
@@ -141,9 +152,9 @@ function ...kubectl.log.all.contexts.perftest() {
 alias .kubectl.log.all.contexts.perftest.tofiles='...kubectl.log.all.contexts.perftest.tofiles'
 function ...kubectl.log.all.contexts.perftest.tofiles() {
   .kubectl.context.perftest.00
-  kubectl logs -l 'app.kubectl.io/name in (em-hrs-api-java,em-hrs-ingestor-java)' --tail=-1 > perf.00.log.txt
+  kubectl logs -l 'app.kubernetes.io/name in (em-hrs-api-java,em-hrs-ingestor-java)' --tail=-1 > perf.00.log.txt
   .kubectl.context.perftest.01
-  kubectl logs -l 'app.kubectl.io/name in (em-hrs-api-java,em-hrs-ingestor-java)' --tail=-1 > perf.01.log.txt
+  kubectl logs -l 'app.kubernetes.io/name in (em-hrs-api-java,em-hrs-ingestor-java)' --tail=-1 > perf.01.log.txt
   cat perf.00.log.txt perf.01.log.txt | sort > perf.txt
   echo "logging all"
 }
