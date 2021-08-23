c219f6a057aa611a9d89c709835d933b437aac63
Paul Crimes <paul.crimes@solirius.com>
2021-08-23 17:10:22 +0100

    fix preview 01 al

--- bash/aliases/azure.sh
+++ bash/aliases/azure.sh
@@ -11,7 +11,7 @@ alias .az.credentials.demo.01='az aks get-credentials --resource-group demo-01-r
 alias .az.credentials.perftest.00='az aks get-credentials --resource-group cft-perftest-00-rg --name cft-perftest-00-aks --subscription DCD-CFTAPPS-TEST --overwrite'
 alias .az.credentials.perftest.01='az aks get-credentials --resource-group cft-perftest-01-rg --name cft-perftest-01-aks --subscription DCD-CFTAPPS-TEST --overwrite'
 alias .az.credentials.preview.00='az aks get-credentials --resource-group preview-00-rg --name preview-00-aks --subscription DCD-CFTAPPS-DEV --overwrite'
-alias .az.credentials.preview='az aks get-credentials --resource-group preview-01-rg --name preview-01-aks --subscription DCD-CFTAPPS-DEV --overwrite'
+alias .az.credentials.preview.01='az aks get-credentials --resource-group preview-01-rg --name preview-01-aks --subscription DCD-CFTAPPS-DEV --overwrite'
 
 alias .az.credentials.prod.00='az aks get-credentials --resource-group prod-00-rg --name prod-00-aks --subscription DCD-CFTAPPS-PROD --overwrite'
 alias .az.credentials.prod.01='az aks get-credentials --resource-group prod-01-rg --name prod-01-aks --subscription DCD-CFTAPPS-PROD --overwrite'
