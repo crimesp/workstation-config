18cf1f1ab869a4ebcc734f162bbce9d6215736f4
Paul Crimes <paul.crimes@solirius.com>
2021-09-02 09:40:28 +0100

    stop unnatended upgrades

--- bash/aliases/jobs.sh
+++ bash/aliases/jobs.sh
@@ -15,3 +15,13 @@ echo "Running statement: $STATEMENT"
 "$COMMAND" &> ${LOGFILE} &
 }
 
+
+#graceful shutdown of apt during stuck unattended upgrades
+function .systemctl.stop-unattended-upgrades () {
+systemctl list-units --type=service | grep upgr;
+sudo systemctl stop unattended-upgrades;
+systemctl list-units --type=service | grep upgr;
+
+
+
+}
