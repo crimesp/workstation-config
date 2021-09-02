8406a48098add141f9bdf95e9db4f48476c62687
Paul Crimes <paul.crimes@solirius.com>
2021-09-02 17:25:59 +0100

    add stitching aat

--- bash/aliases/postgres.sh
+++ bash/aliases/postgres.sh
@@ -1,16 +1,21 @@
-function .postgres.tunnel.create.aat() {
+#HRS
+
+
+function .postgres.tunnel.create.hrs.aat() {
 # you can get this from the portal, or determine it via the inputs your pass to this module in your code
 
 POSTGRES_DB_SUBDOMAIN=em-hrs-api-postgres-v11-db
 POSTRGRES_DB_ENV=aat
 
 POSTGRES_HOST="${POSTGRES_DB_SUBDOMAIN}-${POSTRGRES_DB_ENV}.postgres.database.azure.com"
-echo "TUNNELLING TO ${POSTGRES_HOST} - open a new terminal to connect"
+echo "TUNNELLING TO ${POSTGRES_HOST} - PLEASE LEAVE THIS TERMINAL OPEN, and open a new terminal to connect using .postgres.connect.hrs.aat"
 ssh -N bastion-nonprod.platform.hmcts.net -L 5440:${POSTGRES_HOST}:5432
 # expect no more output in this terminal you won't get an interactive prompt
 }
 
-function .postgres.connect.aat() {
+#you will need postgres client
+#psql "sslmode=require host=localhost port=5440 dbname=${DB_NAME} user=${DB_USER}"
+function .postgres.connect.hrs.aat() {
 
 # in a separate terminal run:
 export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query accessToken -o tsv)
@@ -27,3 +32,42 @@ DB_USER="DTS\ CFT\ DB\ Access\ Reader@${POSTGRES_DB_SUBDOMAIN}-${POSTRGRES_DB_EN
 
 psql "sslmode=require host=localhost port=5440 dbname=${DB_NAME} user=${DB_USER}"
 }
+
+
+
+
+#STITCHING
+
+function .postgres.tunnel.create.stitching.aat() {
+# you can get this from the portal, or determine it via the inputs your pass to this module in your code
+
+POSTGRES_DB_SUBDOMAIN=em-stitching-postgres-db
+POSTRGRES_DB_ENV=aat
+
+POSTGRES_HOST="${POSTGRES_DB_SUBDOMAIN}-${POSTRGRES_DB_ENV}.postgres.database.azure.com"
+echo "TUNNELLING TO ${POSTGRES_HOST} - PLEASE LEAVE THIS TERMINAL OPEN, and open a new terminal to connect using .postgres.connect.stitching.aat"
+ssh -N bastion-nonprod.platform.hmcts.net -L 5440:${POSTGRES_HOST}:5432
+# expect no more output in this terminal you won't get an interactive prompt
+}
+
+#you will need postgres client
+#psql "sslmode=require host=localhost port=5440 dbname=${DB_NAME} user=${DB_USER}"
+function .postgres.connect.stitching.aat() {
+
+# in a separate terminal run:
+export PGPASSWORD=$(az account get-access-token --resource-type oss-rdbms --query accessToken -o tsv)
+# this matches the `database_name` parameter you pass in the module
+DB_NAME=emstitching
+
+POSTGRES_DB_SUBDOMAIN=em-stitching-postgres-db
+POSTRGRES_DB_ENV=aat
+
+
+# Update the suffix after the @ to the server name
+DB_USER="DTS\ CFT\ DB\ Access\ Reader@${POSTGRES_DB_SUBDOMAIN}-${POSTRGRES_DB_ENV}" # read access
+#DB_USER="DTS\ Platform\ Operations@rpe-draft-store-aat" # operations team administrative access
+
+psql "sslmode=require host=localhost port=5440 dbname=${DB_NAME} user=${DB_USER}"
+}
+
+
