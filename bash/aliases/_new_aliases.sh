dd9acf2be939b719db7ab946c7f9a7a4f977dcc6
Paul Crimes <paul.crimes@solirius.com>
2021-09-22 08:32:09 +0100

    add clam scan

--- bash/aliases/_new_aliases.sh
+++ bash/aliases/_new_aliases.sh
@@ -3,3 +3,8 @@
 alias .git.reset.head='git reset HEAD .'
 
 alias .clam.scan.full='clamscan / -v -r --exclude-dir .*ecryptfs.*"'
+
+
+alias .cpu.monitor='cpufreq-info  | grep current; .cpu.governor.show'
+
+alias .cpu.governor.show='cat /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor'
--- bash/aliases/cd.sh
+++ bash/aliases/cd.sh
@@ -9,7 +9,7 @@ alias .cd.idea.projects='cd ~/idea/'
 alias .cd.multi='cd ~/idea/multiproject'
 
 alias .cd.em='cd ~/git/em'
-alias .cd.hrs='cd ~/git/em/em-hrs-api'
+alias .cd.api='cd ~/git/em/em-hrs-api'
 alias .cd.ingestor='cd ~/git/em/em-hrs-ingestor'
 alias .cd.docstore='cd ~/git/em/document-management-store-app'
 alias .cd.flux='cd ~/git/em/cnp-flux-config'
