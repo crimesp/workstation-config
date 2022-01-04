be6d9f49fed8de991b84db955ee19dfaf9dcb62b
Paul Crimes <paul.crimes@solirius.com>
2022-01-04 15:52:50 +0000

    add cd to legacy

--- bash/aliases/cd.sh
+++ bash/aliases/cd.sh
@@ -9,6 +9,7 @@ alias .cd.idea.projects='cd ~/idea/'
 alias .cd.multi='cd ~/idea/multiproject'
 
 alias .cd.em='cd ~/git/em'
+alias .cd.legacy='cd ~/git/legacyapps'
 alias .cd.api='cd ~/git/em/em-hrs-api'
 alias .cd.ingestor='cd ~/git/em/em-hrs-ingestor'
 alias .cd.docstore='cd ~/git/em/document-management-store-app'
