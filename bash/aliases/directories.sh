5e0956fa90fdc0c43d9c341463d51f755215b1c8
Paul Crimes <1911935+crimesp@users.noreply.github.com>
2022-06-11 14:01:28 +0100

    saturday uopdates

--- bash/aliases/directories.sh
+++ bash/aliases/directories.sh
@@ -4,7 +4,21 @@ function .subdirs.exec () {
    for d in ./*/ ;
    do (
        cd "$d" ;
+       echo "About to excute in directory $d"
        exec $@
+       echo "Completed excution in directory $d"
+    ); done
+
+
+}
+
+function .subdirs.exec.bg () {
+   for d in ./*/ ;
+   do (
+       cd "$d" ;
+       echo "About to excute in directory $d"
+       exec $@ &
+       echo "Completed excution in directory $d"
     ); done
 
 
