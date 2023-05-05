078fbef94e2e2992faf74f131f566385e6a441ce
Paul Crimes <1911935+crimesp@users.noreply.github.com>
2023-05-05 12:17:57 +0100

    add ssl cert date check

--- bash/aliases/ssl.sh
+++ bash/aliases/ssl.sh
@@ -4,3 +4,11 @@ alias .ssl.client.connect.to.host.port.443='openssl s_client -port 443 -host'
 alias .ssl.client.connect.to.host.port.8443='openssl s_client -port 8443 -host'
 alias .ssl.client.connect='.ssl.client.connect.to.host.port.443'
 
+function .ssl.checkdates() {
+     if [ -z "$1" ]
+      then
+          echo "Supply a host"
+      return 1
+      fi
+  openssl s_client -port 443 -showcerts -host $1 | openssl x509 -noout -dates
+}
