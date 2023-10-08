627e2604b451978ba74f3e10246371a1cb7b4b47
Paul Crimes <1911935+crimesp@users.noreply.github.com>
2023-10-08 17:10:26 +0100

    new alias

--- bash/aliases/_new_aliases.sh
+++ bash/aliases/_new_aliases.sh
@@ -2,6 +2,7 @@
 alias .screens.ultrawide.only='xrandr --output eDP-1 --off --output DP-1 --auto'
 
 
+alias .ffmpeg='ffmpeg -i simvegas-circuitoverload.wav -acodec mp3 simonvegas-circuitoverload.mp3'
 
 alias .i3.bindings='grep -e '^[^#]*bind' ~/.config/i3/config'
 alias .x.window.class.show='xprop WM_CLASS'
