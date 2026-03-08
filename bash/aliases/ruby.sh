1d8ebc125ee664011655e5f55d9c709c3ec00a81
Paul Crimes <1911935+crimesp@users.noreply.github.com>
2026-03-08 21:34:17 +0000

    video crop

---
+++ bash/aliases/ruby.sh
@@ -0,0 +1,23 @@
+#for mac
+ #brew install rbenv ruby-build
+
+alias .ruby.install='rbenv install'
+alias .ruby.set.global='rbenv global'
+alias .ruby.set.local='rbenv local'
+alias .ruby.versions='rbenv versions'
+
+
+#for jekyll
+#  gem install bundler jekyll
+#
+#  jekyll new my-awesome-site
+#
+#  cd my-awesome-site
+#
+#  bundle exec jekyll serve
+#
+## => Now browse to http://localhost:4000
+
+
+alias .jekyll.new='jekyll new'
+alias .jekyll.serve='bundle exec jekyll serve'
